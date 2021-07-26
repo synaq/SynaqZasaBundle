c67ca83dbcd94bbafe598cfb670c7f488267ff9e
Willem Viljoen <willemv@synaq.com>
2021-07-26 16:12:26 +0200

    Added configuration hooks for auth propagation time parameter

--- DependencyInjection/SynaqZasaExtension.php
+++ DependencyInjection/SynaqZasaExtension.php
@@ -54,5 +54,6 @@ class SynaqZasaExtension extends Extension
         $container->setParameter('synaq_zasa.use_fopen', $config['use_fopen']);
         $container->setParameter('synaq_zasa.auth_token_path', $config['auth_token_path']);
         $container->setParameter('synaq_zasa.rest_base_url', $config['rest_base_url']);
+        $container->setParameter('synaq_zasa.auth_propagation_time', $config['auth_propagation_time']);
     }
 }
